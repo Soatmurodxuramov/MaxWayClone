678a50d965bd93ade8c15bb98f66423abaa59461
soatm <soatmurodxurramov45@gmail.com>
2026-02-21 15:08:42 +0500

    add test

--- app/src/main/java/uz/gita/maxwayclone/data/ApiClient.kt
+++ app/src/main/java/uz/gita/maxwayclone/data/ApiClient.kt
@@ -1,4 +1,21 @@
 package uz.gita.maxwayclone.data
 
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import uz.gita.maxwayclone.app.App.Companion.instance
+
 object ApiClient {
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor (ChuckerInterceptor.Builder(instance).build())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
 }
